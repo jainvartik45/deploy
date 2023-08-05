938a6731bc0ff48245bc114be9225dde4bd997a6
jainvartik45 <84932356+jainvartik45@users.noreply.github.com>
2023-08-05 12:14:03 +0530

    commited

---
+++ build/precache-manifest.17d6d29f232feffee96c74395b193fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980dd028c4181db6eeb43bb3e931fda2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e26c27ecbb18a5db787",
+    "url": "/static/css/main.18cb0288.chunk.css"
+  },
+  {
+    "revision": "12b5325931461fba59e1",
+    "url": "/static/js/2.f6fb3e65.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.f6fb3e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e26c27ecbb18a5db787",
+    "url": "/static/js/main.3cee9de7.chunk.js"
+  },
+  {
+    "revision": "4f6e34b3be3410815ed1",
+    "url": "/static/js/runtime-main.07b96789.js"
+  },
+  {
+    "revision": "0e651ff6c9928dad413931ce4ed2057b",
+    "url": "/static/media/green-chameleon-s9CC2SKySJM-unsplash.0e651ff6.jpg"
+  }
+]);
